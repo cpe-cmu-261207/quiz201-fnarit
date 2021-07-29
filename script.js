43d51a97914560a164c39f9daf1b19ddce9e6d80
fnarit <naritsararat@gmail.com>
2021-07-29 15:49:49 +0700

    update

--- script.js
+++ script.js
@@ -5,9 +5,35 @@ const length = document.getElementById('length')
 const color = document.querySelector('#color')
 
 // define more constants and variables here
+let cal = ''
+let b = document.querySelector("#length").value
+let a = 620612152 + b
 
 btn_toggle.onclick = () => {
   // your code here
+  author.innerHTML = a
 }
 
-// more codes for Search and Reset buttons here
+
+// // more codes for Search and Reset buttons here
+
+const search = document.querySelector("#search")
+const text = document.querySelector("#text")
+const span = document.createElement('span')
+search.onclick = () => {
+  if(text.length > b){
+    span.setAttribute('value','#FF0000')
+    text.append('span')
+  }
+}
+// const changColor = (ev) => {
+//   const text = document.querySelector("#text")
+//   const v = document.querySelector("#number").value
+//   text.style.color = v
+// }
+
+// const text = document.querySelector("#text")
+const v = document.querySelector('#color').value
+color.addEventListener('click', () =>{
+  text.style.color = v
+})
